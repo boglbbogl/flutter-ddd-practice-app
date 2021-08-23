2babab1fc0dc7533162859423cccbf895d862afb
boglbbogl <75574246+boglbbogl@users.noreply.github.com>
2021-08-23 22:29:30 +0900

    t

--- lib/domain/api_example_practice/i_api_example_repository.dart
+++ lib/domain/api_example_practice/i_api_example_repository.dart
@@ -1,7 +1,7 @@
 import 'package:ddd_practice_app/domain/api_example_practice/api_example.dart';
 
 abstract class IApiExampleRepository {
-  Future<List<ApiExample>> fetchExample({
+  Future<Either<ApiExampleFailure, ApiExample>> fetchExample({
     required int limit,
     required int page,
   });
--- lib/infrastructure/api_example/api_example_repository.dart
+++ lib/infrastructure/api_example/api_example_repository.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
+import 'package:dartz/dartz.dart';
 import 'package:ddd_practice_app/domain/api_example_practice/api_example.dart';
+import 'package:ddd_practice_app/domain/api_example_practice/api_example_failure.dart';
 import 'package:ddd_practice_app/domain/api_example_practice/i_api_example_repository.dart';
 import 'package:ddd_practice_app/infrastructure/api_example/api_example_dtos.dart';
 import 'package:injectable/injectable.dart';
@@ -9,7 +11,7 @@ import 'package:http/http.dart' as http;
 @LazySingleton(as: IApiExampleRepository)
 class ApiExampleRepository implements IApiExampleRepository {
   @override
-  Future<List<ApiExample>> fetchExample({
+  Future<Either<ApiExampleFailure,ApiExample>> fetchExample({
     required int limit,
     required int page,
   }) async {
@@ -20,17 +22,19 @@ class ApiExampleRepository implements IApiExampleRepository {
       if (response.statusCode == 200) {
         final decoded = json.decode(utf8.decode(response.bodyBytes))
             as Map<String, dynamic>;
-        final data = decoded["data"] as List<dynamic>;
-        final apiExampleData = data
-            .map((e) =>
-                ApiExampleDto.fromJson(e as Map<String, dynamic>).toDomain())
-            .toList();
-        return apiExampleData;
+        final apiExampleData =
+            ApiExampleDto.fromJson(decoded["data"] as Map<String, dynamic>);
+        // final data = decoded["data"] as List<dynamic>;
+        // final apiExampleData = data
+        //     .map((e) =>
+        //         ApiExampleDto.fromJson(e as Map<String, dynamic>).toDomain())
+        //     .toList();
+        return right(apiExampleData.toDomain());
       } else {
-        return [];
+        return left(const ApiExampleFailure.unexpected());
       }
     } catch (error) {
-      return [];
+      return left(const ApiExampleFailure.serverError());
     }
   }
 }
