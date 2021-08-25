ee2fe6d81d336feacdbc79224625bdce49f1e106
boglbbogl <75574246+boglbbogl@users.noreply.github.com>
2021-08-25 11:02:49 +0900

    t

--- lib/domain/api_example_post_practice/i_api_post_example_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'api_post_example.dart';
-
-abstract class IApiPostExampleRepository {
-  Future<List<ApiPostExample>> postExampleData({
-    required String name,
-    required String job,
-  });
-}
